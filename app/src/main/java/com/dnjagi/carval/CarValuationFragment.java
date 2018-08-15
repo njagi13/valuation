30af5593c9081214ab9267742d5d4647e106d15e
Njagi <njagi13@gmail.com>
2018-08-15 22:22:33 +0300

    carVal

--- app/src/main/java/com/dnjagi/carval/CarValuationFragment.java
+++ app/src/main/java/com/dnjagi/carval/CarValuationFragment.java
@@ -13,10 +13,13 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import com.dnjagi.carval.data.UploadRecord;
+import com.dnjagi.carval.utility.Utilities;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
-
+import java.util.UUID;
 
 public class CarValuationFragment extends Fragment
         implements View.OnClickListener {
@@ -55,18 +58,40 @@ public class CarValuationFragment extends Fragment
     }
 
     Calendar myCalendar = Calendar.getInstance();
-    EditText dateOfRegText , dateOfPolicyExpiryText;
+    EditText dateOfRegText, dateOfPolicyExpiryText;
+
+    private EditText input_make, input_model, input_regnumber, input_color, input_odom, input_yearOfManf, input_policyNo,
+            input_policyexpirydate, input_chasisnumber, input_enginenumber, input_engrating, input_fname, input_lname,
+            input_phone1, input_phone2, input_email;
+
+    UploadRecord mUploadRecord;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_car_valuation, container, false);
-        Button btnNext = (Button) view.findViewById(R.id.buttonNext);
+        mUploadRecord = new UploadRecord();
+        input_make = (EditText) view.findViewById(R.id.input_make);
+        input_model = (EditText) view.findViewById(R.id.input_model);
+        input_regnumber = (EditText) view.findViewById(R.id.input_regnumber);
+        input_color = (EditText) view.findViewById(R.id.input_color);
+        input_odom = (EditText) view.findViewById(R.id.input_odom);
+        input_yearOfManf = (EditText) view.findViewById(R.id.input_yearOfManf);
+        input_policyNo = (EditText) view.findViewById(R.id.input_policyNo);
 
-
+        input_chasisnumber = (EditText) view.findViewById(R.id.input_chasisnumber);
+        input_enginenumber = (EditText) view.findViewById(R.id.input_enginenumber);
+        input_engrating = (EditText) view.findViewById(R.id.input_engrating);
+        input_fname = (EditText) view.findViewById(R.id.input_fname);
+        input_lname = (EditText) view.findViewById(R.id.input_lname);
+        input_phone1 = (EditText) view.findViewById(R.id.input_phone1);
+        input_phone2 = (EditText) view.findViewById(R.id.input_phone2);
+        input_email = (EditText) view.findViewById(R.id.input_email);
         dateOfRegText = (EditText) view.findViewById(R.id.input_dateofreg);
         dateOfPolicyExpiryText = (EditText) view.findViewById(R.id.input_policyexpirydate);
+
+        Button btnNext = (Button) view.findViewById(R.id.buttonNext);
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -162,13 +187,39 @@ public class CarValuationFragment extends Fragment
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonNext:
-                Fragment fragment = null;
-               // fragment = new CameraViewFragment();
-                fragment = new EngineSystemFragment();
-                replaceFragment(fragment);
-                break;
+        try {
+
+            switch (v.getId()) {
+                case R.id.buttonNext:
+                    Fragment fragment = null;
+
+                    mUploadRecord.Make = input_make.getText().toString();
+                    mUploadRecord.Model = input_model.getText().toString();
+                    mUploadRecord.RegistrationNumber = input_regnumber.getText().toString();
+                    mUploadRecord.Color = input_color.getText().toString();
+                    mUploadRecord.OdomReading = input_odom.getText().toString();
+                    mUploadRecord.YearOfManufacture = input_yearOfManf.getText().toString();
+                    mUploadRecord.PolicyNumber = input_policyNo.getText().toString();
+                    mUploadRecord.InsExpiryDate = dateOfPolicyExpiryText.getText().toString();
+                    mUploadRecord.ChassisNumber = input_chasisnumber.getText().toString();
+                    mUploadRecord.EngineNumber = input_enginenumber.getText().toString();
+                    mUploadRecord.EngineRating = input_engrating.getText().toString();
+                    mUploadRecord.FirstName = input_fname.getText().toString();
+                    mUploadRecord.LastName = input_lname.getText().toString();
+                    mUploadRecord.PhoneNumber1 = input_phone1.getText().toString();
+                    mUploadRecord.PhoneNumber2 = input_phone2.getText().toString();
+                    mUploadRecord.Email = input_email.getText().toString();
+                    mUploadRecord.DateOfReg = dateOfRegText.getText().toString();
+                    mUploadRecord.UploadRecordID = UUID.randomUUID();
+                    //save locally
+                    mUploadRecord.save();
+
+                    fragment = new EngineSystemFragment();
+                    replaceFragment(fragment);
+                    break;
+            }
+        } catch (Exception ex) {
+            Utilities.LogException(ex);
         }
 
     }
